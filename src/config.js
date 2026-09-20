819af39159cf3954b47713dab784ac7488249ce4
lucaortolomo <l.ortolomo@gmail.com>
2026-09-20 13:59:46 +0200

    Add production frontend container

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // src/config.js
-const apiBaseUrl = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+const defaultApiBaseUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080';
+const apiBaseUrl = process.env.REACT_APP_API_BASE_URL || defaultApiBaseUrl;
 
 const config = { API_BASE_URL: apiBaseUrl };
 
